cf918f5e858d9ea579805887edf06100a433df43
Ethan Crawford <ethcrawford@gmail.com>
2018-08-06 14:09:17 +0300

    Add Font-Awesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './modules/bootstrap/bootstrap.css';
+import './modules/font-awesome/font-awesome.css';
 import './index.css';
 import './modules/visually-hidden/visually-hidden.css';
 import App from './components/App';
