04b31501e95166292e717183f859c1afb668396d
Ethan Crawford <ethcrawford@gmail.com>
2018-08-06 13:59:44 +0300

    Add Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './modules/bootstrap/bootstrap.css';
 import './index.css';
 import './modules/visually-hidden/visually-hidden.css';
 import App from './components/App';
